3c51c9561f3f94b491d55e33bb30fd86d45304f5
Henry Amos <amoshenry95@gmail.com>
2024-02-24 00:38:00 +0000

    Changes made to index and js file

--- script.js
+++ script.js
@@ -34,3 +34,30 @@ $(document).ready(function() {
 
 
 });
+
+
+function calculateAge() {
+    // User's birthdate
+    const birthDate = new Date('1999-03-07'); 
+    
+    // Current date
+    const currentDate = new Date();
+    
+    // Calculate the user's age
+    let age = currentDate.getFullYear() - birthDate.getFullYear();
+    
+    // Check if the user's birthday has occurred this year
+    const userHasBirthdayThisYear =
+        currentDate.getMonth() > birthDate.getMonth() ||
+        (currentDate.getMonth() === birthDate.getMonth() && currentDate.getDate() >= birthDate.getDate());
+    
+    // If the user's birthday hasn't occurred yet this year, subtract 1 from age
+    if (!userHasBirthdayThisYear) {
+        age--;
+    }
+    
+    // return age;
+    document.getElementById('age').innerHTML += ' ' + age;
+}
+
+calculateAge(); // Output the calculated age
